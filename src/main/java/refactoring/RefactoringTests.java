84504742691653457f3793983ff5a04063f49525
Märt Kalmo <mart.kalmo@gmail.com>
2017-09-27 16:01:41 +0300

    added some comments for refactoring exercise

--- src/main/java/refactoring/RefactoringTests.java
+++ src/main/java/refactoring/RefactoringTests.java
@@ -20,11 +20,21 @@ public class RefactoringTests {
     public void calculatesWeeklyPayWithOvertime() {
         assertThat(sut.calculateWeeklyPay(39, true), is(195));
         assertThat(sut.calculateWeeklyPay(41, true), is(208));
+
+        // testing new methods:
+
+        // assertThat(sut.calculateWeeklyPayWithOvertime(39), is(195));
+        // assertThat(sut.calculateWeeklyPayWithOvertime(41), is(208));
     }
 
     @Test
     public void calculatesWeeklyPayWithoutOvertime() {
         assertThat(sut.calculateWeeklyPay(39, false), is(195));
         assertThat(sut.calculateWeeklyPay(41, false), is(205));
+
+        // testing new methods:
+
+        // assertThat(sut.calculateWeeklyPayWithoutOvertime(39), is(195));
+        // assertThat(sut.calculateWeeklyPayWithoutOvertime(41), is(205));
     }
 }
