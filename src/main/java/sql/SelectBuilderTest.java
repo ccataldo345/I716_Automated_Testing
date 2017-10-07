e2117645130db86b529b0c6aeba064c098d0f853
Märt Kalmo <mart.kalmo@gmail.com>
2017-10-07 10:05:04 +0300

    added new test for sqlBuilder exercise

--- src/main/java/sql/SelectBuilderTest.java
+++ src/main/java/sql/SelectBuilderTest.java
@@ -2,9 +2,9 @@ package sql;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -31,17 +31,27 @@ public class SelectBuilderTest {
     @Test
     public void test3() {
         SelectBuilder b = new SelectBuilder();
+        b.columns("a", "b");
+        b.from("t");
+        b.columns("c");
+
+        assertThat(b.getSql(), is("select a, b, c from t"));
+    }
+
+    @Test
+    public void test4() {
+        SelectBuilder b = new SelectBuilder();
         b.column("a");
         b.from("t");
         b.where("id = ?", 1);
 
         assertThat(b.getSql(), is("select a from t where id = ?"));
-        assertThat(b.getParameters(), is(Arrays.asList(1)));
+        assertThat(b.getParameters(), is(asList(1)));
 
     }
 
     @Test
-    public void test4() {
+    public void test5() {
         SelectBuilder b = new SelectBuilder();
         b.column("a");
         b.from("t");
@@ -53,7 +63,7 @@ public class SelectBuilderTest {
     }
 
     @Test
-    public void test5() {
+    public void test6() {
         SelectBuilder b = new SelectBuilder();
         b.column("a");
         b.from("t");
@@ -62,22 +72,22 @@ public class SelectBuilderTest {
         b.eqIfNotNull("c", 3);
 
         assertThat(b.getSql(), is("select a from t where a = ? and c = ?"));
-        assertThat(b.getParameters(), is(Arrays.asList(1, 3)));
-    }
-
-    @Test
-    public void test6() {
-        SelectBuilder b = new SelectBuilder();
-        b.column("a");
-        b.from("t");
-        b.in("id", Arrays.asList(1, 2));
-
-        assertThat(b.getSql(), is("select a from t where id in (?, ?)"));
-        assertThat(b.getParameters(), is(Arrays.asList(1, 2)));
+        assertThat(b.getParameters(), is(asList(1, 3)));
     }
 
     @Test
     public void test7() {
+        SelectBuilder b = new SelectBuilder();
+        b.column("a");
+        b.from("t");
+        b.in("id", asList(1, 2));
+
+        assertThat(b.getSql(), is("select a from t where id in (?, ?)"));
+        assertThat(b.getParameters(), is(asList(1, 2)));
+    }
+
+    @Test
+    public void test8() {
         SelectBuilder b = new SelectBuilder();
         b.column("a");
         b.from("t");
@@ -87,7 +97,7 @@ public class SelectBuilderTest {
     }
 
     @Test
-    public void test8() {
+    public void test9() {
         SelectBuilder b = new SelectBuilder();
         b.column("a");
         b.from("t");
@@ -97,7 +107,7 @@ public class SelectBuilderTest {
     }
 
     @Test
-    public void test9() {
+    public void test10() {
         SelectBuilder sub = new SelectBuilder();
         sub.column("b");
         sub.from("t");
