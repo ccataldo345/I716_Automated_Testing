ad45cb2711e957af1cc6275c73be77c41b2ccb4a
ccataldo345 <post345@outlook.com>
2018-09-17 15:28:32 +0300

    Exercise 03

--- src/main/java/rpn/RpnCalculatorTest.java
+++ src/main/java/rpn/RpnCalculatorTest.java
@@ -2,6 +2,8 @@ package rpn;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,11 +16,49 @@ public class RpnCalculatorTest {
         assertThat(c.getAccumulator(), is(0));
     }
 
+    @Test
+    public void possibleToSetAccumulator() {
+        RpnCalculator c = new RpnCalculator();
+        c.setAccumulator(1);
+
+        assertThat(c.getAccumulator(), is(1));
+    }
+
+
+    @Test
+    public void calculatorSupportsAddition() {
+        RpnCalculator c = new RpnCalculator();
+        c.setAccumulator(1);
+        c.enter();
+
+        c.setAccumulator(2);
+        c.plus();
+
+        assertThat(c.getAccumulator(), is(3));
+    }
+
 }
 
 class RpnCalculator {
 
-    public Integer getAccumulator() {
-        return null;
+    private int accumulator = 0;
+
+    Stack<Integer> stack = new Stack<>();
+
+    public int getAccumulator() {
+        return accumulator;
     }
+
+    public void setAccumulator(int number) {
+        this.accumulator = number;
+    }
+
+    public void enter() {
+        stack.push(accumulator);
+    }
+
+    public void plus() {
+        accumulator += stack.pop();
+    }
+
 }
