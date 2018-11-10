5f8fbc6aaec634a256d26f6afd2fa113f1f5ed25
Christian Cataldo <christian.cataldo@itcollege.ee>
2018-11-10 07:39:34 +0200

    Exercise 10

--- src/main/java/mockito/TransferServiceTestMockito.java
+++ src/main/java/mockito/TransferServiceTestMockito.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.*;
 
+import custommock.TestableBankService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -27,15 +28,28 @@ public class TransferServiceTestMockito {
         // transfer 1 EUR from account E_123 to account S_456
         // E_123 accounts currency is EUR
         // S_456 accounts currency is SEK
+        when(bankService.getAccountCurrency("S_456")).thenReturn("SEK");
+        when(bankService.convert(new Money(1, "EUR"), "SEK")).
+                thenReturn(new Money(10, "SEK"));
+        when(bankService.convert(new Money(1, "EUR"), "EUR")).
+                thenReturn(new Money(1, "EUR"));
+        when(bankService.hasSufficientFundsFor(new Money(1, "EUR"), "E_123")).
+                thenReturn(true);
+
         transferService.transfer(new Money(1, "EUR"), "E_123", "S_456");
 
         verify(bankService).withdraw(new Money(1, "EUR"), "E_123");
-        // verify(bankService).deposit(...
+        verify(bankService).deposit(new Money(10, "SEK"), "S_456");
     }
 
     @Test
     public void transferWhenNotEnoughFunds() {
         // ...
+        transferService.transfer(new Money(1, "EUR"), "E_123", "S_456");
+
+        verify(bankService, never()).withdraw(anyMoney(), anyAccount());
+        verify(bankService, never()).deposit(anyMoney(), anyAccount());
+
     }
 
     private Money anyMoney() {
