4c26504dae75a8f17004ffd5724d1703a5a836c7
Christian Cataldo <christian.cataldo@itcollege.ee>
2018-11-17 12:38:35 +0200

    Exercise 11

--- src/main/java/string/OrderService.java
+++ src/main/java/string/OrderService.java
@@ -1,11 +1,14 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class OrderService {
 
     private DataSource dataSource;
+    private List<Order> filledOrders = new ArrayList<>();
 
     public OrderService(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -16,14 +19,40 @@ public class OrderService {
     }
 
     public List<Order> getFilledOrders() {
-        throw new IllegalStateException("not implemented");
+        //throw new IllegalStateException("not implemented");
+        // returns orders on which isFilled() returns true
+
+        List<Order> orders = dataSource.getOrders();
+
+        for (Order order : orders) {
+            if (order.isFilled()) {
+                filledOrders.add(order);
+            }
+        }
+        return filledOrders;
     }
 
     public List<Order> getOrdersOver(double amount) {
-        throw new IllegalStateException("not implemented");
+        // throw new IllegalStateException("not implemented");
+
+        List<Order> orders = dataSource.getOrders();
+
+        for (Order order : orders) {
+            if (order.getTotal() > 100.0) {
+                filledOrders.add(order);
+            }
+        }
+        return filledOrders;
     }
 
     public List<Order> getOrdersSortedByDate() {
-        throw new IllegalStateException("not implemented");
+        // throw new IllegalStateException("not implemented");
+
+        List<Order> orders = dataSource.getOrders();
+
+        orders.sort(Comparator.comparing(Order::getOrderDate));
+        // ref: https://stackoverflow.com/questions/16252269/how-to-sort-an-arraylist
+
+        return orders;
     }
 }
--- src/main/java/string/OrderServiceTest.java
+++ src/main/java/string/OrderServiceTest.java
@@ -15,46 +15,67 @@ public class OrderServiceTest {
     @Test
     public void shouldFindFilledOrders() {
         assertThat(numbersAsString(service.getFilledOrders()),
-                   is("N123, N124"));
+                is("N123, N124"));
     }
 
     @Test
     public void shouldFindOrdersOverOverCertainAmount() {
         assertThat(amountsAsString(service.getOrdersOver(100.0)),
-                   is("..."));
+                   is("N124, N125"));
     }
 
     @Test
     public void shouldFindOrdersSortedByDate() {
         assertThat(datesAsString(service.getOrdersSortedByDate()),
-                   is("..."));
+                   is("N124, N125, N123"));
     }
 
     private String numbersAsString(List<Order> filledOrders) {
-        return "not implemented";
+        // return "not implemented";
+
+        List<String> orders = new ArrayList<>();
+        for(Order order: filledOrders) {
+            orders.add(order.getNumber());
+        }
+        return String.join(", ", orders);
     }
 
     private String amountsAsString(List<Order> filledOrders) {
-        return "not implemented";
+        // return "not implemented";
+
+        List<String> orders = new ArrayList<>();
+        for(Order order: filledOrders) {
+            orders.add(order.getNumber());
+        }
+        return String.join(", ", orders);
     }
 
     private String datesAsString(List<Order> filledOrders) {
-        return "not implemented";
+        // return "not implemented";
+
+        List<String> orders = new ArrayList<>();
+        for(Order order: filledOrders) {
+            orders.add(order.getNumber());
+        }
+        return String.join(", ", orders);
     }
 
     private OrderService getOrderService() {
         // here is test data defined
 
+        // total price: 27.2
         final Order order1 = new Order("N123", asDate("2013-11-21"));
         order1.addItem(new LineItem("pen", 0.6, 4));
         order1.addItem(new LineItem("paper", 3.1, 8));
         order1.setFilled(true);
 
+        // total price: 290
         final Order order2 = new Order("N124", asDate("1991-10-06"));
         order2.addItem(new LineItem("e-reader", 100, 2));
         order2.addItem(new LineItem("book", 30, 3));
         order2.setFilled(true);
 
+        // total price: 1025
         final Order order3 = new Order("N125", asDate("2000-07-06"));
         order3.addItem(new LineItem("cd", 5, 5));
         order3.addItem(new LineItem("desk", 100, 10));
