3825d6424bd7355e742f604d0218f5ffb8d5b429
Christian Cataldo <christian.cataldo@itcollege.ee>
2018-11-21 10:29:28 +0200

    Exercise 12

--- src/main/java/person/PersonBuilder.java
+++ src/main/java/person/PersonBuilder.java
@@ -2,11 +2,36 @@ package person;
 
 public class PersonBuilder {
 
+    private int age;
+    private String gender;
+    private Address address;
+    private String name;
+
     public PersonBuilder withAge(int age) {
-        throw new IllegalStateException("not implemented");
+        // throw new IllegalStateException("not implemented");
+        this.age = age;
+        // System.out.println(this);    // return object
+        // System.out.println(this.age);    // return age of object
+        return this;
+    }
+
+    public PersonBuilder withGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public PersonBuilder withTown(String town) {
+        Address address = new Address(null, town);
+        address.setTown(town);
+        this.address = address;
+        return this;
     }
 
     public Person build() {
-        throw new IllegalStateException("not implemented");
+        // throw new IllegalStateException("not implemented");
+        Person person = new Person(name, age, gender, address);
+        person.setAddress(address);
+        return person;
     }
+
 }
--- src/main/java/person/PersonUtilTest.java
+++ src/main/java/person/PersonUtilTest.java
@@ -22,20 +22,33 @@ public class PersonUtilTest {
 
     @Test
     public void findsPersonsInLegalAge() {
-        fail("not implemented");
+        Person p1 = aPerson().withAge(12).build();
+        Person p2 = aPerson().withAge(33).build();
+
+        assertThat(personUtil.getPersonsInLegalAge(asList(p1, p2)), is(asList(p2)));
     }
 
     @Test
     public void findsWomen() {
-        fail("not implemented");
+        //fail("not implemented");
+
+        Person p1 = aPerson().withGender("M").build();
+        Person p2 = aPerson().withGender("F").build();
+
+        assertThat(personUtil.getWomen(asList(p1, p2)), is(asList(p2)));
     }
 
     @Test
     public void findsPersonsLivingInSpecifiedTown() {
-        fail("not implemented");
+        // fail("not implemented");
+        Person p1 = aPerson().withTown("Tallinn").build();
+        Person p2 = aPerson().withTown("Tartu").build();
+
+        assertThat(personUtil.getPersonsWhoLiveIn("Tallinn", asList(p1, p2)), is(asList(p1)));
     }
 
     private PersonBuilder aPerson() {
         return new PersonBuilder();
     }
+
 }
