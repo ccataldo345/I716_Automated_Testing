8ecce39a2e0e36b39071448f06087dc3f6213b7e
Christian Cataldo <christian.cataldo@itcollege.ee>
2018-12-15 22:35:58 +0200

    Lab 2

--- src/main/java/invoice/InvoiceRowGenerator.java
+++ src/main/java/invoice/InvoiceRowGenerator.java
@@ -1,38 +1,123 @@
+
 package invoice;
 
+
 import java.math.BigDecimal;
-import java.text.NumberFormat;
+import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class InvoiceRowGenerator {
-
+    private static final int PAYMENT_DAY = 10;
+    private static final int MIN_PAYMENT = 3;
     private InvoiceRowDao invoiceRowDao;
-    // private BigDecimal amount = BigDecimal.valueOf(100);
-    // private LocalDate dateStart;
-    // private LocalDate dateEnd;
 
-
-    // constructor
     public InvoiceRowGenerator(InvoiceRowDao dao) {
         this.invoiceRowDao = dao;
     }
 
-    public void generateInvoiceRows(BigDecimal amount, LocalDate periodStart, LocalDate periodEnd) {
-        // https://stackoverflow.com/questions/3395825/how-to-print-formatted-bigdecimal-values
-        //String myAmount = NumberFormat.getCurrencyInstance().format(amount);
-        //System.out.println("Total amount is: " + myAmount );
+    public List<InvoiceRow> generateInvoiceRows(BigDecimal amount, LocalDate periodStart, LocalDate periodEnd) {
+        validateAmount(amount);
+        validateDateOrder(periodStart, periodEnd);
 
-        List<InvoiceRow> invoiceRows = new ArrayList<>();
+        List<LocalDate> dates = getPaymentDays(periodStart, periodEnd);
+        List<BigDecimal> amounts = getAmounts(amount, BigDecimal.valueOf(dates.size()));
+        List<InvoiceRow> invoiceRows = getListOfInstallments(dates, amounts);
 
-        invoiceRows.add(new InvoiceRow(amount, periodStart));
-        invoiceRows.add(new InvoiceRow(amount, periodEnd));
-
-        for (InvoiceRow i: invoiceRows) {
-
-            invoiceRowDao.save(i);
+        for (InvoiceRow invoiceRow : invoiceRows) {
+            invoiceRowDao.save(invoiceRow);
+            System.out.println(invoiceRow);
         }
 
+        return invoiceRows;
+    }
+
+    private List<InvoiceRow> getListOfInstallments(List<LocalDate> dates, List<BigDecimal> payments) {
+        List<InvoiceRow> installments = new ArrayList<>();
+        for (int i = 0; i < payments.size(); i++) {
+            InvoiceRow installment = new InvoiceRow(payments.get(i), dates.get(i));
+            installments.add(installment);
+        }
+        return installments;
+    }
+
+    private List<BigDecimal> getAmounts(BigDecimal amount, BigDecimal numberOfPayments) {
+        List<BigDecimal> payments = new ArrayList<>();
+
+        if (amount.intValue() < MIN_PAYMENT) {
+            //if amount less than minimal payment so we have only one payment equal to amount
+            payments.add(amount);
+        } else {
+            //Getting one payment by dividing the amount on the number of payments
+            //RoundingMode.DOWN - when you divide two integers have the same behavior
+            BigDecimal onePayment = amount.divide(numberOfPayments, RoundingMode.DOWN);
+            while (onePayment.intValue() < MIN_PAYMENT) {
+                /*if a value of one payment less than minimal payment we should decrease
+                the number of payments until one payment became more or equals to minimal
+                payment */
+                numberOfPayments = numberOfPayments.subtract(BigDecimal.valueOf(1));
+                onePayment = amount.divide(numberOfPayments, RoundingMode.DOWN);
+            }
+            //now we can generate all payments
+            payments = generateAmounts(onePayment, numberOfPayments.intValue());
+
+            int remainder = amount.remainder(numberOfPayments).intValue();
+            if (remainder > 0) {
+                //if we have remainder we should distribute it between other payments
+                updateAmounts(remainder, payments);
+            }
+        }
+        return payments;
+    }
+
+    private List<BigDecimal> generateAmounts(BigDecimal onePayment, int numberOfPayments) {
+        List<BigDecimal> payments = new ArrayList<>();
+        for (int i = 0; i < numberOfPayments; i++) {
+            payments.add(onePayment);
+        }
+        return payments;
+    }
+
+    private List<BigDecimal> updateAmounts(int remainder, List<BigDecimal> payments) {
+        for (int i = 0; i < remainder; i++) {
+            BigDecimal updatedValue = payments.get(i).add(BigDecimal.valueOf(1));
+            payments.set(i, updatedValue);
+        }
+        Collections.reverse(payments);
+        return payments;
+    }
+
+    private List<LocalDate> getPaymentDays(LocalDate periodStart, LocalDate periodEnd) {
+        List<LocalDate> dates = new ArrayList<>();
+        LocalDate firstDateOfPayment = getFirstDateOfPayment(periodStart);
+        dates.add(firstDateOfPayment);
+
+        LocalDate nextPaymentDate = firstDateOfPayment.plusMonths(1).withDayOfMonth(PAYMENT_DAY);
+        while (nextPaymentDate.isBefore(periodEnd) || nextPaymentDate.isEqual(periodEnd)) {
+            dates.add(nextPaymentDate);
+            nextPaymentDate = nextPaymentDate.plusMonths(1);
+        }
+        return dates;
+    }
+
+    private void validateAmount(BigDecimal amount) {
+        if (amount.intValue() < 0) {
+            throw new IllegalArgumentException(
+                    "The amount is negative!");
+        }
+    }
+
+    private void validateDateOrder(LocalDate periodStart, LocalDate periodEnd) {
+        if (periodStart.isAfter(periodEnd)) {
+            throw new IllegalArgumentException(
+                    "The start period date is after the end period date!");
+        }
+    }
+
+    private LocalDate getFirstDateOfPayment(LocalDate periodStart) {
+        if (periodStart.isBefore(periodStart.withDayOfMonth(PAYMENT_DAY))) {
+            return periodStart.withDayOfMonth(PAYMENT_DAY);
+        }
+        return periodStart;
     }
 }
--- src/main/java/invoice/InvoiceRowGeneratorTest.java
+++ src/main/java/invoice/InvoiceRowGeneratorTest.java
@@ -1,25 +1,22 @@
 package invoice;
 
-import org.apache.xalan.templates.ElemValueOf;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import sun.awt.SunHints;
 
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class InvoiceRowGeneratorTest {
-
     @Mock InvoiceRowDao dao;
 
     @Test
@@ -27,7 +24,7 @@ public class InvoiceRowGeneratorTest {
 
         BigDecimal amount = new BigDecimal(100);
         LocalDate periodStart = asDate("2017-01-01");
-        // LocalDate periodEnd = asDate("2017-02-02");
+        // LocalDate periodEnd = asDate("2017-02-02"); >>> NO
         LocalDate periodEnd = asDate("2017-02-10");
 
         InvoiceRowGenerator generator = new InvoiceRowGenerator(dao);
@@ -35,21 +32,18 @@ public class InvoiceRowGeneratorTest {
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
         // check that it produced two rows with dates 2017-01-01 and 2017-02-01 >>> NO
-        //CC check that it produced two rows with dates 2017-01-10 and 2017-02-10
+        // CC: check that it produced two rows with dates 2017-01-10 and 2017-02-10
 
         // Which data would be written to the database that can be
         // tested by checking the calls and arguments to the mocked
         // InvoiceRowDao.save() method.
 
-        dao.save(new InvoiceRow(amount, periodStart));
-        dao.save(new InvoiceRow(amount, periodEnd));
+        LocalDate expectedFirstDate = asDate("2017-01-10");
+        LocalDate expectedSecondDate = asDate("2017-02-10");
 
-        //verify(dao).save(argThat(row -> row.amount.intValue() == 100));
-
-        //verify(dao).save(getMatcherForAmount(100));
-
-        //verify(dao).save(getMatcherForAmountWithMessage(100));
-
+        verify(dao, times(2)).save(anyInvoiceRow());
+        verify(dao, times(1)).save(getMatcherForDateWithMessage(expectedFirstDate));
+        verify(dao, times(1)).save(getMatcherForDateWithMessage(expectedSecondDate));
     }
 
     @Test
@@ -57,17 +51,13 @@ public class InvoiceRowGeneratorTest {
         dao.save(new InvoiceRow(new BigDecimal(1), LocalDate.now()));
 
         verify(dao).save(argThat(row -> row.amount.intValue() == 1));
-
         verify(dao).save(getMatcherForAmount(1));
-
         verify(dao).save(getMatcherForAmountWithMessage(1));
     }
 
-    // CC TESTS:
-
+    // CC Tests 1-5 check dates; tests 6-10 check amounts
     @Test
     public void periodStartsOn10th_verifyFirstInstallmentDateIsOn10th() {
-
         // test 01
         BigDecimal amount = new BigDecimal(10);
         LocalDate periodStart = asDate("2017-01-10");
@@ -76,24 +66,35 @@ public class InvoiceRowGeneratorTest {
 
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
+        LocalDate expectedFirstDate = asDate("2017-01-10");
+        LocalDate expectedSecondDate = asDate("2017-02-10");
+
+        verify(dao, times(2)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForDateWithMessage(expectedFirstDate));
+        verify(dao).save(getMatcherForDateWithMessage(expectedSecondDate));
     }
 
     @Test
     public void periodStartsAfter10th_verifyFirstInstallmentDateIsOnPeriodStart() {
-
         // test 02
         BigDecimal amount = new BigDecimal(10);
         LocalDate periodStart = asDate("2017-01-11");
         LocalDate periodEnd = asDate("2017-02-10");
         InvoiceRowGenerator generator = new InvoiceRowGenerator(dao);
 
+
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
+        LocalDate expectedFirstDate = asDate("2017-01-11");
+        LocalDate expectedSecondDate = asDate("2017-02-10");
+
+        verify(dao, times(2)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForDateWithMessage(expectedFirstDate));
+        verify(dao).save(getMatcherForDateWithMessage(expectedSecondDate));
     }
 
     @Test
     public void periodStartsBefore10th_verifyFirstInstallmentDateIsOn10th() {
-
         // test 03
         BigDecimal amount = new BigDecimal(10);
         LocalDate periodStart = asDate("2017-01-09");
@@ -102,11 +103,16 @@ public class InvoiceRowGeneratorTest {
 
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
+        LocalDate expectedFirstDate = asDate("2017-01-10");
+        LocalDate expectedSecondDate = asDate("2017-02-10");
+
+        verify(dao, times(2)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForDateWithMessage(expectedFirstDate));
+        verify(dao).save(getMatcherForDateWithMessage(expectedSecondDate));
     }
 
     @Test
     public void periodEndsBefore10th_verifyNoInstallmentInPeriodEndMonth() {
-
         // test 04
         BigDecimal amount = new BigDecimal(10);
         LocalDate periodStart = asDate("2017-01-10");
@@ -115,11 +121,14 @@ public class InvoiceRowGeneratorTest {
 
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
+        LocalDate expectedDate = asDate("2017-01-10");
+
+        verify(dao, times(1)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForDateWithMessage(expectedDate));
     }
 
     @Test
     public void periodSpansOverYearBoundary_verifyNoBugsInPayments() {
-
         // test 05
         BigDecimal amount = new BigDecimal(10);
         LocalDate periodStart = asDate("2017-12-01");
@@ -128,11 +137,21 @@ public class InvoiceRowGeneratorTest {
 
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
+        LocalDate expectedFirstDate = asDate("2017-12-10");
+        LocalDate expectedSecondDate = asDate("2018-01-10");
+        LocalDate expectedThirdDate = asDate("2018-02-10");
+
+        verify(dao, times(3)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForDateWithMessage(expectedFirstDate));
+        verify(dao).save(getMatcherForDateWithMessage(expectedSecondDate));
+        verify(dao).save(getMatcherForDateWithMessage(expectedThirdDate));
     }
+
+
+    // CC Tests 6-10 check amounts only
 
     @Test
     public void amountDividesEqually() {
-
         // test 06
         BigDecimal amount = new BigDecimal(9);
         LocalDate periodStart = asDate("2017-01-10");
@@ -141,11 +160,12 @@ public class InvoiceRowGeneratorTest {
 
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
+        verify(dao, times(3)).save(anyInvoiceRow());
+        verify(dao, times(3)).save(getMatcherForAmountWithMessage(3));
     }
 
     @Test
     public void amountNotDividesEqually_verifyReminderIsAddedToLastInstallment() {
-
         // test 07
         BigDecimal amount = new BigDecimal(7);
         LocalDate periodStart = asDate("2017-01-10");
@@ -156,11 +176,13 @@ public class InvoiceRowGeneratorTest {
 
         // check that produced 2 rows with amounts 3 and 4
 
+        verify(dao, times(2)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForAmountWithMessage(3));
+        verify(dao).save(getMatcherForAmountWithMessage(4));
     }
 
     @Test
     public void amountNotDivideEqually_verifyReminderIsDividedBetweenLastInstallments() {
-
         // test 08
         BigDecimal amount = new BigDecimal(11);
         LocalDate periodStart = asDate("2017-01-10");
@@ -170,11 +192,13 @@ public class InvoiceRowGeneratorTest {
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
         // check that produced three rows with amounts 3, 4 and 4
+        verify(dao, times(3)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForAmountWithMessage(3));
+        verify(dao, times(2)).save(getMatcherForAmountWithMessage(4));
     }
 
     @Test
     public void installmentMustBeAtLeast3Euro_verifySmallerInstallmentsAreNotPayed() {
-
         // test 09
         BigDecimal amount = new BigDecimal(6);
         LocalDate periodStart = asDate("2017-01-10");
@@ -184,13 +208,14 @@ public class InvoiceRowGeneratorTest {
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
         // check that produced 2 rows with amounts 3 and 3
+        verify(dao, times(2)).save(anyInvoiceRow());
+        verify(dao, times(2)).save(getMatcherForAmountWithMessage(3));
     }
 
     @Test
     public void paymentIsLessThan3Euros_verifyTheOnlyInstallmentIsEqualToPayment() {
-
         // test 10
-        BigDecimal amount = new BigDecimal(11);
+        BigDecimal amount = new BigDecimal(2);
         LocalDate periodStart = asDate("2017-01-10");
         LocalDate periodEnd = asDate("2017-03-10");
         InvoiceRowGenerator generator = new InvoiceRowGenerator(dao);
@@ -198,10 +223,12 @@ public class InvoiceRowGeneratorTest {
         generator.generateInvoiceRows(amount, periodStart, periodEnd);
 
         // check that produced 1 row with amount 2
+        verify(dao, times(1)).save(anyInvoiceRow());
+        verify(dao).save(getMatcherForAmountWithMessage(2));
     }
 
     @BeforeEach
-    public void initMocks() {
+    void initMocks() {
         MockitoAnnotations.initMocks(this);
     }
 
@@ -224,15 +251,35 @@ public class InvoiceRowGeneratorTest {
 
             @Override
             public String toString() {
-                return MessageFormat.format(
-                        "InvoiceRow with amount {0}", amount);
+                return MessageFormat.format("InvoiceRow with amount {0}", amount);
             }
         });
+    }
+
+    private InvoiceRow getMatcherForDateWithMessage(final LocalDate date) {
+        // Example matcher for testing that argument has certain date.
+        // With message
+
+        return argThat(new ArgumentMatcher<InvoiceRow>() {
+
+            @Override
+            public boolean matches(InvoiceRow row) {
+                return date.isEqual(row.date);
+            }
+
+            @Override
+            public String toString() {
+                return MessageFormat.format("InvoiceRow with date {0}", date);
+            }
+        });
+    }
+
+    private InvoiceRow anyInvoiceRow() {
+        return ArgumentMatchers.any();
     }
 
     private LocalDate asDate(String string) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return LocalDate.parse(string, formatter);
     }
-
 }
